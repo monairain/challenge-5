698776eec33c308b1a851363c6c23069fee20e9a
Memouna Irain <memounairain@Memounas-MacBook-Pro.local>
2023-08-29 23:20:41 +0100

    updated js and README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # challenge-5
-Module 5 Javascript challenge
+
+Module 5 Javascript challenge: Password Generator
+
+## Description
+
+Build a password generator with the following requirements:
+
+* Generate a password when the button is clicked.
+
+* Present a series of prompts for password criteria:
+    -   Length of password:
+    -   At least 8 characters but no more than 128.
+    -   Character types:
+    -   Lowercase
+    -   Uppercase
+    -   Numeric
+    -   Special characters ($@%&*, etc.)
+
+Code should validate for each input and at least one character type should be selected.
+
+Once all prompts are answered, the password should be generated and displayed in an alert or written to the page.
+
+## Usage
+
+![Password Generator Screenshot](/assets/Screenshot%202023-08-29%20at%2023.10.40.png)
+
+## Review
+
+* The URL of the deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
--- script.js
+++ script.js
@@ -90,17 +90,106 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  //variable to store length of pw from user input
+  let length = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  )
+
+  // Check if password length is valid
+  if (isNaN(length) || length < 10 || length >= 64) {
+    alert("Password length must be between 10 and 63 characters.");
+    return;
+  }
+
+// Ask user about character types to include
+let hasSpecialCharacters = confirm(
+  "Include special characters"
+)
+
+let hasNumericCharacters = confirm(
+  "Include numeric characters"
+)
+
+let hasLowerCasedCharacters = confirm(
+  "Include lowercase characters"
+)
+
+let hasUpperCasedCharacters = confirm(
+  "Include uppercase characters"
+)
+
+// Check if at least one character type is selected
+if (hasLowerCasedCharacters === false &&
+   hasUpperCasedCharacters === false &&
+   hasSpecialCharacters === false &&
+   hasNumericCharacters === false) {
+     alert(`Must select at least one character type`);
+     return;
+  }
+
+// Create options object
+let passwordOptions = {
+  length: length,
+  hasLowerCasedCharacters: hasLowerCasedCharacters,
+  hasUpperCasedCharacters: hasUpperCasedCharacters,
+  hasSpecialCharacters: hasSpecialCharacters,
+  hasNumericCharacters: hasNumericCharacters
+}
+
+return passwordOptions;
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  // Get user's password options
+  let options = getPasswordOptions();
+  console.log(options);
+  
+  // Initialize variables
+  let result = []
+  let possibleCharacter = []
+  let guaranteedCharacter = []
 
+  // Concatenate character arrays based on user's options
+  if (options.hasSpecialCharacters) {
+    possibleCharacter = possibleCharacter.concat(specialCharacters);
+    guaranteedCharacter.push(getRandom(specialCharacters))
+  }
+
+  if (options.hasLowerCasedCharacters) {
+    possibleCharacter = possibleCharacter.concat(lowerCasedCharacters);
+    guaranteedCharacter.push(getRandom(lowerCasedCharacters))
+  }
+
+  if (options.hasUpperCasedCharacters) {
+    possibleCharacter = possibleCharacter.concat(upperCasedCharacters);
+    guaranteedCharacter.push(getRandom(upperCasedCharacters))
+  }
+
+  if (options.hasNumericCharacters) {
+    possibleCharacter = possibleCharacter.concat(numericCharacters);
+    guaranteedCharacter.push(getRandom(numericCharacters))
+  }
+
+  console.log(possibleCharacter);
+  
+  // Generate the password
+  for(let index = 0; index < options.length; index++){
+  var generated = getRandom(possibleCharacter);
+  console.log(generated);
+  result.push(generated);
+  }
+
+console.log(result);
+
+return result.join("")
 }
 
 // Get references to the #generate element
